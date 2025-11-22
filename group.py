9d039d6561172b038d672a1c68c37274ed77ac3a
akn620 <ucapak3@ucl.ac.uk>
2025-11-22 22:04:15 +0000

    Group dictionary formed with member info

--- group.py
+++ group.py
@@ -2,4 +2,17 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {"members": [
+            {"name": "Jill", "age": 26, "Job": "Biologist",},
+            {"name": "Zalika", "age": 28, "Job": "Artist", },
+            {"name": "John", "age": 27, "Job": "Writer", },
+            {"name": "Nash", "age": 34, "Job": "Chef", }],
+
+            "relationships": [    
+                {"type": "partners", "persons": ("Jill", "Zalika")},
+                {"type": "friends", "persons": ("Jill", "John")},
+                {"type": "cousins", "persons": ("Nash", "John")},
+                {"type": "landlord", "landlord": "Nash", "tenant": "Zalika"}]
+}
+
+print(my_group["relationships"][3]["tenant"]) # Output: Zalika
