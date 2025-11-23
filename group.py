c9407695036f26d1204f9a4c53b895eb18580ccb
akn620 <ucapak3@ucl.ac.uk>
2025-11-23 00:19:44 +0000

    Adds functions to group information

--- group.py
+++ group.py
@@ -2,17 +2,58 @@
 
 # Your code to go here...
 
-my_group = {"members": [
-            {"name": "Jill", "age": 26, "Job": "Biologist",},
-            {"name": "Zalika", "age": 28, "Job": "Artist", },
-            {"name": "John", "age": 27, "Job": "Writer", },
-            {"name": "Nash", "age": 34, "Job": "Chef", }],
-
-            "relationships": [    
-                {"type": "partners", "persons": ("Jill", "Zalika")},
-                {"type": "friends", "persons": ("Jill", "John")},
-                {"type": "cousins", "persons": ("Nash", "John")},
-                {"type": "landlord", "landlord": "Nash", "tenant": "Zalika"}]
+group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
 }
+def max_age(group):
+    return max(member["age"] for member in group.values())  
+print(max_age(group))
 
-print(my_group["relationships"][3]["tenant"]) # Output: Zalika
+def ave_relations(group):
+    total_relations = sum(len(member["relations"]) for member in group.values())
+    return total_relations / len(group)
+print(ave_relations(group))
+
+def rel_max_age(group):
+    rel_over_1 = [member for member in group.values() if len(member["relations"]) >= 1]
+    max_age_rel = max(member["age"] for member in rel_over_1)
+    return max_age_rel   
+print(rel_max_age(group))
+
+def friend_max_age(group):
+    friend = [member for member in group.values() if "friend" in member["relations"].values()]
+    max_age_friend = max(member["age"] for member in friend)
+    return max_age_friend
+print(friend_max_age(group))
+
+
+
